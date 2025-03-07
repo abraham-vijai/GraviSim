a4a3d5f2252cce7bf15a59723bce794dcf8048aa
Abraham Vijai <abrahamvijai03@gmail.com>
2025-03-07 13:38:49 -0500

    Added comments

--- GraviSim/Ball.h
+++ GraviSim/Ball.h
@@ -3,20 +3,36 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 
+/**
+ * @brief Represents a ball with physics properties such as position, velocity, gravity, and damping.
+ */
 class Ball {
 public:
-    glm::vec3 position;
-    glm::vec2 velocity;
-    float radius;
-    float gravity = -9.81f;
-    float damping = 0.8f;
-    float velocityThreshold = 0.01f;
-    int segments;
+    glm::vec3 position; ///< Position of the ball in 3D space.
+    glm::vec2 velocity; ///< Velocity of the ball in 2D (x, y) plane.
+    float radius; ///< Radius of the ball.
+    float gravity = -9.81f; ///< Gravity affecting the ball's motion.
+    float damping = 0.8f; ///< Damping factor applied during collisions.
+    float velocityThreshold = 0.01f; ///< Minimum velocity below which movement stops.
+    int segments; ///< Number of segments used to approximate the circle.
 
+    /**
+     * @brief Constructs a Ball object with initial position, velocity, radius, and resolution.
+     *
+     * @param pos Initial position of the ball.
+     * @param vel Initial velocity of the ball.
+     * @param r Radius of the ball.
+     * @param res Number of segments for circle approximation.
+     */
     Ball(glm::vec3 pos, glm::vec2 vel, float r, int res)
         : position(pos), velocity(vel), radius(r), segments(res) {
     }
 
+    /**
+     * @brief Updates the ball's physics including position and velocity.
+     *
+     * @param deltaTime Time step for the physics update.
+     */
     void updatePhysics(float deltaTime) {
         // Apply gravity if above ground
         if (position.y - radius > -1.0f)
@@ -29,13 +45,18 @@ public:
         handleCollisions();
     }
 
+    /**
+     * @brief Generates the vertex data for rendering the ball as a 2D circle.
+     *
+     * @param circleVertices Vector to store generated vertex data.
+     */
     void generateBallVertices(std::vector<float>& circleVertices) {
         // Center position of the circle
         circleVertices.push_back(0.0f);
         circleVertices.push_back(0.0f);
         circleVertices.push_back(0.0f);
 
-        // Generate circle circleVertices
+        // Generate circle vertices
         for (int i = 0; i <= segments; i++) {
             float angle = (2.0f * glm::pi<float>() * i) / segments;
             float x = radius * cos(angle);
@@ -47,21 +68,27 @@ public:
     }
 
 private:
+    /**
+     * @brief Handles ball collisions with boundaries and applies response forces.
+     */
     void handleCollisions() {
+        // Collision with left or right wall
         if (position.x + radius >= 1.0f || position.x - radius <= -1.0f) {
             velocity.x *= -damping;
             position.x = glm::clamp(position.x, -1.0f + radius, 1.0f - radius);
             if (fabs(velocity.x) < velocityThreshold) velocity.x = 0.0f;
         }
 
-        if (position.y - radius <= -1.0f) { // Ground collision
+        // Collision with the ground
+        if (position.y - radius <= -1.0f) {
             velocity.y *= -damping;
             position.y = -1.0f + radius;
             velocity.x *= damping;  // Apply ground friction
             if (fabs(velocity.y) < velocityThreshold) velocity.y = 0.0f;
             if (fabs(velocity.x) < velocityThreshold) velocity.x = 0.0f;
         }
-        else if (position.y + radius >= 1.0f) { // Ceiling collision
+        // Collision with the ceiling
+        else if (position.y + radius >= 1.0f) {
             velocity.y *= -damping;
             position.y = 1.0f - radius;
             if (fabs(velocity.y) < velocityThreshold) velocity.y = 0.0f;
@@ -69,5 +96,4 @@ private:
     }
 };
 
-
-#endif // !BALL_H
+#endif
--- GraviSim/Shader.h
+++ GraviSim/Shader.h
@@ -11,33 +11,37 @@
 class Shader
 {
 public:
-    unsigned int ID;
+    unsigned int ID; // ID of the shader program
 
-    // constructor generates the shader on the fly
-    // ------------------------------------------------------------------------
+    /**
+     * @brief Constructor that generates the shader program on the fly.
+     *
+     * @param vertexPath Path to the vertex shader source file.
+     * @param fragmentPath Path to the fragment shader source file.
+     */
     Shader(const char* vertexPath, const char* fragmentPath)
     {
-        // 1. retrieve the vertex/fragment source code from filePath
+        // 1. Retrieve the vertex/fragment source code from filePath
         std::string vertexCode;
         std::string fragmentCode;
         std::ifstream vShaderFile;
         std::ifstream fShaderFile;
-        // ensure ifstream objects can throw exceptions:
+        // Ensure ifstream objects can throw exceptions
         vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
         fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
         try
         {
-            // open files
+            // Open files
             vShaderFile.open(vertexPath);
             fShaderFile.open(fragmentPath);
             std::stringstream vShaderStream, fShaderStream;
-            // read file's buffer contents into streams
+            // Read file's buffer contents into streams
             vShaderStream << vShaderFile.rdbuf();
             fShaderStream << fShaderFile.rdbuf();
-            // close file handlers
+            // Close file handlers
             vShaderFile.close();
             fShaderFile.close();
-            // convert stream into string
+            // Convert stream into string
             vertexCode = vShaderStream.str();
             fragmentCode = fShaderStream.str();
         }
@@ -47,93 +51,182 @@ public:
         }
         const char* vShaderCode = vertexCode.c_str();
         const char* fShaderCode = fragmentCode.c_str();
-        // 2. compile shaders
+        // 2. Compile shaders
         unsigned int vertex, fragment;
-        // vertex shader
+        // Vertex shader
         vertex = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertex, 1, &vShaderCode, NULL);
         glCompileShader(vertex);
         checkCompileErrors(vertex, "VERTEX");
-        // fragment Shader
+        // Fragment shader
         fragment = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fragment, 1, &fShaderCode, NULL);
         glCompileShader(fragment);
         checkCompileErrors(fragment, "FRAGMENT");
-        // shader Program
+        // Shader program
         ID = glCreateProgram();
         glAttachShader(ID, vertex);
         glAttachShader(ID, fragment);
         glLinkProgram(ID);
         checkCompileErrors(ID, "PROGRAM");
-        // delete the shaders as they're linked into our program now and no longer necessary
+        // Delete the shaders as they're linked into our program now and no longer necessary
         glDeleteShader(vertex);
         glDeleteShader(fragment);
     }
-    // activate the shader
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Activates the shader program.
+     */
     void use()
     {
         glUseProgram(ID);
     }
-    // utility uniform functions
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets a boolean uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param value Boolean value to set.
+     */
     void setBool(const std::string& name, bool value) const
     {
         glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
     }
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets an integer uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param value Integer value to set.
+     */
     void setInt(const std::string& name, int value) const
     {
         glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
     }
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets a float uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param value Float value to set.
+     */
     void setFloat(const std::string& name, float value) const
     {
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
     }
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets a 2-component vector uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param value glm::vec2 value to set.
+     */
     void setVec2(const std::string& name, const glm::vec2& value) const
     {
         glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
     }
+
+    /**
+     * @brief Sets a 2-component vector uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param x First component of the vector.
+     * @param y Second component of the vector.
+     */
     void setVec2(const std::string& name, float x, float y) const
     {
         glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
     }
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets a 3-component vector uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param value glm::vec3 value to set.
+     */
     void setVec3(const std::string& name, const glm::vec3& value) const
     {
         glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
     }
+
+    /**
+     * @brief Sets a 3-component vector uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param x First component of the vector.
+     * @param y Second component of the vector.
+     * @param z Third component of the vector.
+     */
     void setVec3(const std::string& name, float x, float y, float z) const
     {
         glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
     }
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets a 4-component vector uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param value glm::vec4 value to set.
+     */
     void setVec4(const std::string& name, const glm::vec4& value) const
     {
         glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
     }
+
+    /**
+     * @brief Sets a 4-component vector uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param x First component of the vector.
+     * @param y Second component of the vector.
+     * @param z Third component of the vector.
+     * @param w Fourth component of the vector.
+     */
     void setVec4(const std::string& name, float x, float y, float z, float w) const
     {
         glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
     }
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets a 2x2 matrix uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param mat glm::mat2 value to set.
+     */
     void setMat2(const std::string& name, const glm::mat2& mat) const
     {
         glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     }
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets a 3x3 matrix uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param mat glm::mat3 value to set.
+     */
     void setMat3(const std::string& name, const glm::mat3& mat) const
     {
         glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     }
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets a 4x4 matrix uniform in the shader.
+     *
+     * @param name Name of the uniform variable.
+     * @param mat glm::mat4 value to set.
+     */
     void setMat4(const std::string& name, const glm::mat4& mat) const
     {
         glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     }
+
 private:
+    /**
+     * @brief Checks for shader compilation or linking errors.
+     *
+     * @param shader Shader or program ID to check.
+     * @param type Type of shader or program ("VERTEX", "FRAGMENT", or "PROGRAM").
+     */
     void checkCompileErrors(unsigned int shader, std::string type)
     {
         int success;
@@ -156,6 +249,7 @@ private:
                 std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
             }
         }
-    }};
+    }
+};
 
 #endif
